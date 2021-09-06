e41327c47a798d3fe3e55639c522fc49213abd67
TanjaGagic <57789556+TanjaGagic@users.noreply.github.com>
2021-09-06 19:35:04 +0200

    frontpage syntax changed

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -31,7 +31,6 @@
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
 
             <script type="text/javascript">
-            
             $(document).ready(function(){
                 $('a#button').click(function(){
                     $.ajax({
